606d5d818adbe65cc6da35e257b4047b4fd9fe7c
Makar Osokin <elmortem@gmail.com>
2024-05-19 22:44:59 +0300

    Bugfix

--- IDIContainer.cs
+++ IDIContainer.cs
@@ -1,16 +1,7 @@
-using System;
-
 namespace DI
 {
 	// ReSharper disable once InconsistentNaming
-	public interface IDIContainer : IDIInjector, IDIResolver
+	public interface IDIContainer : IDIMaker, IDIResolver, IDIInjector
 	{
-		T Create<T>(params object[] parameters);
-		object Create(Type type, params object[] parameters);
-		T CreateWithoutFactory<T>(params object[] parameters);
-		object CreateWithoutFactory(Type type, params object[] parameters);
-		T CreateByFactory<T>(params object[] parameters);
-		object CreateByFactory(Type type, params object[] parameters);
-		void Destroy(object obj);
 	}
 }
